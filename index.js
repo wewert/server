fceff0aea55de27622ff2488a653f1a1d6338843
Ken Lee <wewert@gmail.com>
2019-03-19 15:41:39 -0600

    first quarter

---
+++ index.js
@@ -0,0 +1,41 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test5', { useNewUrlParser: true });
+
+const Todo = mongoose.model('Todo', {
+  text: String,
+  complete: Boolean
+});
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+  type Todo {
+    id: ID!
+    text: String!,
+    complete: Boolean!
+  }
+  type Mutation {
+    createTodo(text: String!): Todo
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+  },
+  Mutation: {
+    createTodo: async (_, { text }) => {
+      const todo = new Todo({ text, complete: false});
+      await todo.save();
+      return todo;
+    }
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+mongoose.connection.once('open', function() {
+server.start(() => console.log('Server is running on localhost:4000'))
+});
